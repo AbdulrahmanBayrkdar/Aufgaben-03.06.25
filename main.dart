6ccbc0b6ac641e2afdc05003de94912761f1623a
Abdulrahman Bayrkdar <98399554+AbdulrahmanBayrkdar@users.noreply.github.com>
2025-06-03 11:25:52 +0200

    neue test

---
+++ main.dart
@@ -0,0 +1,17 @@
+void main() {
+  String gdStr = "Guten";
+  String dayStr = "Tag";
+  String worldStr = "Welt";
+  int d = 20;
+  String timeStr = "Zeit";
+  int seven = 7;
+  int twelve = 12;
+  String d = "i";
+  int a = 24;
+  String c = "Schöne";
+  String morning = "Morgen";
+  String S = "Heute";
+  String spa = " ";
+
+  print(gdStr + " " + dayStr);
+}
